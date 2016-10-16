436f717cc939703417c8dbc2a29150c06ddfa830
Zachary Gibson <zach.gibson@buzznog.com>
2016-10-16 14:35:42 -0500

    Adds back sidebar markup

--- web/app/themes/Stardust/templates/sidebar.php
+++ web/app/themes/Stardust/templates/sidebar.php
@@ -1,4 +1,28 @@
 <div style="text-align: center;">
+  <div style="position: relative;">
+    <div class="profile-image-border"></div>
+    <img class="profile-image" src="<?php the_field('profile_image', 'option'); ?>">
+  </div>
+  <p class="bio-title"><?php the_field('name', 'option'); ?></p>
+  <p class="bio-copy"><?php the_field('bio', 'option'); ?></p>
+  <div>
+    <?php if( have_rows('social_links', 'option') ): ?>
+
+      <div class="gucci">
+
+        <?php while( have_rows('social_links', 'option') ): the_row();
+          $image = get_sub_field('image');
+        ?>
+          <a href="<?php the_sub_field('link'); ?>">
+            <img class="social-icon" src="<?php echo $image['url']; ?>" />
+          </a>
+        <?php endwhile; ?>
+
+      </div>
+
+    <?php endif; ?>
+  </div>
+  <div><img class="sidebar-divider" src="<?= get_template_directory_uri(); ?>/dist/images/divider.svg"></div>
   <h4 class="instafeed-title">Instagram</h4>
   <div id="instafeed"></div>
 </div>
