1077ba9934a2752c54b5a2016b223ce256785361
Zachary Gibson <zach.gibson@buzznog.com>
2016-10-24 13:55:02 -0500

    Fixes naming issue and lack of filter

--- web/app/themes/Stardust/lib/post-navigation.php
+++ web/app/themes/Stardust/lib/post-navigation.php
@@ -1,1 +1,31 @@
-<?php the_posts_pagination( array( 'mid_size' => 2 ) ); ?>
+<?php
+  function post_navigation( $args = array() ) {
+    $navigation = '';
+
+    // Don't print empty markup if there's only one page.
+    if ( $GLOBALS['wp_query']->max_num_pages > 1 ) {
+      $args = wp_parse_args( $args, array(
+        'prev_text'          => __( 'GUCCI Older posts' ),
+        'next_text'          => __( 'Newer posts GUCCI' ),
+        'screen_reader_text' => __( 'Posts navigation' ),
+      ) );
+
+      $next_link = get_previous_posts_link( $args['next_text'] );
+      $prev_link = get_next_posts_link( $args['prev_text'] );
+
+      if ( $prev_link ) {
+        $navigation .= '<div class="nav-previous">' . $prev_link . '</div>';
+      }
+
+      if ( $next_link ) {
+        $navigation .= '<div class="nav-next">' . $next_link . '</div>';
+      }
+
+      $navigation = _navigation_markup( $navigation, 'posts-navigation', $args['screen_reader_text'] );
+    }
+
+    return $navigation;
+  }
+
+  add_filter('get_the_posts_navigation', 'post_navigation');
+?>
