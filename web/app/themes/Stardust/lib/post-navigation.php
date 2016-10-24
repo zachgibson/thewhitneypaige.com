4ef8cd900a4b5b11018e965df260411d96897c3b
Zachary Gibson <zach.gibson@buzznog.com>
2016-10-24 13:10:06 -0500

    Uses correct 'posts_nav_link' versus next and previous post for navigating through posts

--- web/app/themes/Stardust/lib/post-navigation.php
+++ web/app/themes/Stardust/lib/post-navigation.php
@@ -1,8 +1,5 @@
-<div class="post-navigation">
-  <div class="alignleft">
-    <?php previous_post_link('&larr; %', 'Toward The Past: ', 'no'); ?>
-  </div>
-  <div class="alignright">
-    <?php next_post_link('% &rarr; ', 'Toward The Future: ', 'no'); ?>
-  </div>
-</div> <!-- end navigation -->
+<div class="navigation">
+  <p>
+    <?php posts_nav_link('','&larr; Newer Posts','Older Posts &rarr;'); ?>
+  </p>
+</div>
