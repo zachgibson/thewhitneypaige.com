008f95ac3f2b6ca424f21d453cc50b649e288d98
Zachary Gibson <zach.gibson@buzznog.com>
2016-10-24 13:24:15 -0500

    Hopefully fixes next/prev page links for posts

--- web/app/themes/Stardust/lib/post-navigation.php
+++ web/app/themes/Stardust/lib/post-navigation.php
@@ -1,5 +1,36 @@
-<div class="navigation">
-  <p>
-    <?php posts_nav_link('','&larr; Newer Posts','Older Posts &rarr;'); ?>
-  </p>
-</div>
+<?php
+function getPrevNext(){
+  $pagelist = get_pages('sort_column=menu_order&sort_order=asc');
+  $pages = array();
+  foreach ($pagelist as $page) {
+     $pages[] += $page->ID;
+  }
+
+  $current = array_search(get_the_ID(), $pages);
+  $prevID = $pages[$current-1];
+  $nextID = $pages[$current+1];
+  
+  echo '<div class="navigation">';
+  
+  if (!empty($prevID)) {
+    echo '<div class="alignleft">';
+    echo '<a href="';
+    echo get_permalink($prevID);
+    echo '"';
+    echo 'title="';
+    echo get_the_title($prevID); 
+    echo'">← Previous</a>';
+    echo "</div>";
+  }
+  if (!empty($nextID)) {
+    echo '<div class="alignright">';
+    echo '<a href="';
+    echo get_permalink($nextID);
+    echo '"';
+    echo 'title="';
+    echo get_the_title($nextID); 
+    echo'">Next →</a>';
+    echo "</div>";    
+  }
+} 
+?>
