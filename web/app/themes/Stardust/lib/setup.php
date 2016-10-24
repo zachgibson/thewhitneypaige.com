d0f1f1f787b0ba07b1f248dedd476b67e2ce3229
Zachary Gibson <zach.gibson@buzznog.com>
2016-10-24 14:05:22 -0500

    Removes sidebar from About and Contact pages

--- web/app/themes/Stardust/lib/setup.php
+++ web/app/themes/Stardust/lib/setup.php
@@ -84,6 +84,7 @@ function display_sidebar() {
     // The sidebar will NOT be displayed if ANY of the following return true.
     // @link https://codex.wordpress.org/Conditional_Tags
     is_404(),
+    is_page( array( 'about', 'contact' ),
     // is_front_page(),
     is_page_template('template-custom.php'),
   ]);
