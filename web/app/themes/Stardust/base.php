433952f4e92cbf7c17ed7338094559a2e249f282
Zachary Gibson <zach.gibson@buzznog.com>
2016-10-24 12:43:32 -0500

    Adds outer container div

--- web/app/themes/Stardust/base.php
+++ web/app/themes/Stardust/base.php
@@ -18,16 +18,18 @@ use Roots\Sage\Wrapper;
       do_action('get_header');
       get_template_part('templates/header');
     ?>
-    <div class="container" role="document">
-      <main class="main">
-        <?php include Wrapper\template_path(); ?>
-      </main><!-- /.main -->
-      <?php if (Setup\display_sidebar()) : ?>
-        <aside class="sidebar">
-          <?php include Wrapper\sidebar_path(); ?>
-        </aside><!-- /.sidebar -->
-      <?php endif; ?>
-    </div><!-- /.wrap -->
+    <div class="outer-container">
+      <div class="container" role="document">
+        <main class="main">
+          <?php include Wrapper\template_path(); ?>
+        </main><!-- /.main -->
+        <?php if (Setup\display_sidebar()) : ?>
+          <aside class="sidebar">
+            <?php include Wrapper\sidebar_path(); ?>
+          </aside><!-- /.sidebar -->
+        <?php endif; ?>
+      </div><!-- /.wrap -->
+    </div>
     <?php
       do_action('get_footer');
       get_template_part('templates/footer');
