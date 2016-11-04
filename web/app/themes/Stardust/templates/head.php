5fdbd6827e43553e24e6f0bd54d69458cb8de66d
Zachary Gibson <zach.gibson@buzznog.com>
2016-11-04 10:44:24 -0500

    Adds OG meta tags

--- web/app/themes/Stardust/templates/head.php
+++ web/app/themes/Stardust/templates/head.php
@@ -2,5 +2,10 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta property="og:url" content="<?php the_permalink() ?>" />
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content="<?php the_title() ?>" />
+  <meta property="og:description" content="<?php the_excerpt() ?>" />
+  <meta property="og:image" content="<?php echo wp_get_attachment_url(get_post_thumbnail_id()) ?>" />
   <?php wp_head(); ?>
 </head>
