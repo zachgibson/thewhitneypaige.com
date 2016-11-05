52821d080965ed7f62b016ff59c0e1820103210b
Zachary Gibson <zach.gibson@buzznog.com>
2016-11-05 13:05:32 -0500

    Uses SVG markup versus image for up arrow

--- web/app/themes/Stardust/base.php
+++ web/app/themes/Stardust/base.php
@@ -55,7 +55,17 @@ use Roots\Sage\Wrapper;
       </div><!-- /.wrap -->
     </div>
     <a class="up" href="#gucci">
-      <img src="<?= get_template_directory_uri(); ?>/dist/images/up-arrow.svg">
+      <svg width="39" height="39" viewBox="0 0 39 39" xmlns="http://www.w3.org/2000/svg">
+        <title>
+          up arrow
+        </title>
+        <g fill="none" fill-rule="evenodd">
+          <circle fill="#FF92D1" cx="19.5" cy="19.5" r="19.5"/>
+          <g fill="#FFF">
+            <path d="M21 19.91l1.885 2.093c.554.616 1.503.666 2.118.112.616-.554.666-1.503.112-2.118l-4.5-5c-.596-.663-1.634-.663-2.23 0l-4.5 5c-.554.615-.504 1.564.112 2.118.615.554 1.564.504 2.118-.112L18 19.91V30c0 .828.672 1.5 1.5 1.5S21 30.828 21 30V19.91zM13.5 9c-.828 0-1.5.672-1.5 1.5s.672 1.5 1.5 1.5h12c.828 0 1.5-.672 1.5-1.5S26.328 9 25.5 9h-12z"/>
+          </g>
+        </g>
+      </svg>
     </a>
     <div class="footer-container">
       <div id="instagramFooterFeed"></div>
