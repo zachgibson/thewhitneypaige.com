fe5fceb8374e1b214920231f8e39ca21302d4f08
Zachary Gibson <zach.gibson@buzznog.com>
2016-11-10 11:09:41 -0600

    Adds target attributes for social links

--- web/app/themes/Stardust/templates/footer.php
+++ web/app/themes/Stardust/templates/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div><span>&copy; <?php echo date('Y'); ?> Whitney Paige 👽</span></div> 
     <div class="follow">
-      <span>Follow me on <a href="https://twitter.com/thewhitneypaige">Twitter</a> &bull; <a href="https://www.instagram.com/thewhitneypaige/">Instagram</a> &bull; <a href="https://www.snapchat.com/add/thewhitneypaige">Snapchat</a></span>
+      <span>Follow me on <a href="https://twitter.com/thewhitneypaige" target="_blank">Twitter</a> &bull; <a href="https://www.instagram.com/thewhitneypaige/" target="_blank">Instagram</a> &bull; <a href="https://www.snapchat.com/add/thewhitneypaige" target="_blank">Snapchat</a></span>
     </div>
 </footer>
