e0bfda1409e56cfd245a380c6e4400450f9f009a
Zachary Gibson <zach.gibson@buzznog.com>
2016-11-13 18:55:12 -0600

    Adds favicon

--- web/app/themes/Stardust/templates/head.php
+++ web/app/themes/Stardust/templates/head.php
@@ -7,5 +7,6 @@
   <meta property="og:title" content="<?php the_title() ?>" />
   <meta property="og:description" content="<?php the_excerpt() ?>" />
   <meta property="og:image" content="<?php echo wp_get_attachment_url(get_post_thumbnail_id()) ?>" />
+  <link rel="icon" href="<?= get_template_directory_uri(); ?>/assets/images/favicon.ico" />
   <?php wp_head(); ?>
 </head>
